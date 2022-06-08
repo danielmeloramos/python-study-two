eb6137842c39aa8f3ae2e5dfa6c8bddf0babfe94
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-08 15:29:53 -0300

    Criando excecoes

---
+++ Examples/long_name_exception.py
@@ -0,0 +1,2 @@
+class LongNameException(Exception):
+    pass
--- Examples/main.py
+++ Examples/main.py
@@ -1,8 +1,13 @@
-#lancamento de excecoes
+from long_name_exception import LongNameException
+
+#1- lancamento de excecoes
 x = -1
 
 if x < 0:
     raise Exception("O número é menor que 0")
 
+#2 Criando excecoes
+long_name = "Está é uma string muito longa para um nome de alguém"
 
-        
+if len(long_name) > 50:
+    raise LongNameException("O nome é muito longo")
