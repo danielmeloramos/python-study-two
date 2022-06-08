06871ae7f7eb450dc34b40f9795e518e5caa8b6c
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-08 18:09:20 -0300

    Realizado exemplos de uso ref a List Comprehensions

---
+++ Examples/example_6/main.py
@@ -0,0 +1,8 @@
+#1- Sem List Comprehensions - Exempplo 1
+
+numbers = [2, 4, 6, 8]
+new = []
+for number in numbers:
+    new.append(number * 2)
+
+print(new)   
---
+++ Examples/example_7/main.py
@@ -0,0 +1,5 @@
+#1- Com List Comprehensions - Exempplo 2
+
+numbers = [2, 4, 6, 8]
+new = [num * 2 for num in numbers]
+print(new)
