fccade56c05ba9e9d5b6f63fce57deddc8fc0eab
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-09 11:29:44 -0300

    Realizado exemplo de uso do map, com e sem o mesmo.

---
+++ Examples/example_15/main.py
@@ -0,0 +1,9 @@
+#1- Map
+
+#Sem map
+items = [1,2,3,4,5]
+squared = []
+for x in items:
+    squared.append(x ** 2)
+
+print(squared)
---
+++ Examples/example_16/main.py
@@ -0,0 +1,7 @@
+#1- Map
+
+#Com map
+items = [1,2,3,4,5]
+squared = map(lambda x: x ** 2, items)
+
+print(list(squared))
