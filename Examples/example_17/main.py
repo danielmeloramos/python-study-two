09994225707e8f3a13d17279c3c4b88d2079ad0f
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-09 11:39:16 -0300

    Criado exemplo de reduce, com e sem ele.

---
+++ Examples/example_17/main.py
@@ -0,0 +1,9 @@
+#1- Reduce
+
+#Sem reduce
+product = 1
+my_list = [1, 2, 3, 4, 5]
+for num in my_list:
+    product = product * num
+
+print(product)
---
+++ Examples/example_18/main.py
@@ -0,0 +1,8 @@
+#1- Reduce
+
+#Com reduce
+from functools import reduce
+
+my_list = [1, 2, 3, 4, 5]
+product = reduce(lambda x, y: x * y, my_list)
+print(product)
