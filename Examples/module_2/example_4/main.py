e1162b4707d5c2f4e7dd9a70dc3f65be60bfd005
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-09 17:53:28 -0300

    Realizado exemplo de uso com kwargs

---
+++ Examples/module_2/example_4/main.py
@@ -0,0 +1,13 @@
+#1- kwargs - unpacking
+
+def main(name, last_name, age):
+    print(f"Full name: {name} {last_name}")
+    print(f"Age: {age}")
+
+data = {
+    "name": "Joao",
+    "last_name": "Filho",
+    "age": 30
+}
+
+print(main(**data))
